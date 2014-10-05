26630f20492063351456981b8cecbe38179f106d
dfreerksen <dfreerksen@gmail.com>
2014-10-05 16:30:59 -0700

    First commit

---
+++ lib/camelotcms/frontend/version.rb
@@ -0,0 +1,5 @@
+module Camelotcms
+  module Pages
+    VERSION = '0.0.1'
+  end
+end
---
+++ lib/camelotcms/pages.rb
@@ -0,0 +1,7 @@
+require 'camelotcms/pages/version'
+
+module Camelotcms
+  module Pages
+    # Your code goes here...
+  end
+end
